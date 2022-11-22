0c5cb40af5980c3008dff35cbe4d964262ac890f
voronovski <avoronovski@gmail.com>
2022-11-21 23:43:18 -0800

    IBOutlets and IBActions added

--- ColorSliders/ViewController.swift
+++ ColorSliders/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: IBOutlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    // MARK: View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
     }
 
-
+    // MARK: IBActions
+    @IBAction func redSliderAction(_ sender: UISlider) {
+    }
+    
+    @IBAction func greenSliderAction(_ sender: UISlider) {
+    }
+    
+    @IBAction func blueSliderAction(_ sender: UISlider) {
+    }
 }
 
