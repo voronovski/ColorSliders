7a8e884f475953c35ef6332789f53f62e0aac134
voronovski <avoronovski@gmail.com>
2022-11-27 17:16:36 -0800

    code optimization

--- ColorSliders/ViewController.swift
+++ ColorSliders/ViewController.swift
@@ -25,25 +25,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redValueLabel.text = String(format: "%.2f", redSlider.value)
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
-        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
-        
         colorView.layer.cornerRadius = 20
-        colorView.backgroundColor = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
+        setRGBLabelsValue()
+        setViewColor()
     }
 
     // MARK: IBActions
     @IBAction func rgbSliderAction(_ sender: UISlider) {
+        setRGBLabelsValue()
+        setViewColor()
+    }
+    
+    // MARK: Private methods
+    private func setRGBLabelsValue() {
         redValueLabel.text = String(format: "%.2f", redSlider.value)
         greenValueLabel.text = String(format: "%.2f", greenSlider.value)
         blueValueLabel.text = String(format: "%.2f", blueSlider.value)
-        
+    }
+    
+    private func setViewColor() {
         colorView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
